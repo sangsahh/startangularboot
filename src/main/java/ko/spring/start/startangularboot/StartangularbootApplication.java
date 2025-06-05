f420cf7f03d390e3e22ec4dc6f66bcf93432970b
202504_kimsg <tomato.202504.kimsg@gmail.com>
2025-06-05 09:30:29 +0900

    Initial commit

---
+++ src/main/java/ko/spring/start/startangularboot/StartangularbootApplication.java
@@ -0,0 +1,13 @@
+package ko.spring.start.startangularboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartangularbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartangularbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ko/spring/start/startangularboot/StartangularbootApplicationTests.java
@@ -0,0 +1,13 @@
+package ko.spring.start.startangularboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartangularbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
